cfe7f2eedd040b32fb1b6f7eb677c95f283ca14b
conao3 <conao3@gmail.com>
2020-04-01 03:12:20 +0900

    fill paragraph

--- outline-toc.el
+++ outline-toc.el
@@ -34,17 +34,18 @@
 
 ;;; Commentary:
 
-;; This provides a sidebar buffer which shows a "table of contents" for an
-;; associated outline-mode buffer. Basically, this shows you the sections of the
-;; outline-mode buffer, but not the bodies. This is to help you remember where
-;; you are in a large document.
+;; This provides a sidebar buffer which shows a "table of
+;; contents" for an associated outline-mode buffer.  Basically,
+;; this shows you the sections of the outline-mode buffer, but
+;; not the bodies.  This is to help you remember where you are in
+;; a large document.
 
-;; Simply use M-x outline-toc-mode to toggle activation of the outline-toc.
-;; Use 'M-x customize-group RET outline-toc RET' to adapt outline-toc to your
-;; needs.
+;; Simply use M-x outline-toc-mode to toggle activation of the
+;; outline-toc.  Use 'M-x customize-group RET outline-toc RET' to
+;; adapt outline-toc to your needs.
 
-;; Much of this was originally adapated from David Engster's excellent
-;; minimap.el (https://github.com/dengste/minimap).
+;; Much of this was originally adapated from David Engster's
+;; excellent minimap.el (https://github.com/dengste/minimap).
 
 ;;; Code:
 
